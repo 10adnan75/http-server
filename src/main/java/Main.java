9bcadc6e3e8dc8afec6c9f22fb4f5c78a740ae4b
Adnan Mazharuddin Shaikh <adnanmazharuddinshaikh@gmail.com>
2025-04-18 00:30:35 -0700

    Extract URL path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,27 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+
     System.out.println("Logs from your program will appear here!");
 
     try {
       ServerSocket serverSocket = new ServerSocket(4221);
-    
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
     
-      Socket socket = serverSocket.accept(); // Wait for connection from client.
-      socket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+      Socket socket = serverSocket.accept(); 
+      BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      String line = reader.readLine();
+      System.out.println(line);
+
+      String[] HTTPRequest = line.split(" ", 0);
+      String op = HTTPRequest[1].equals("/") ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 Not Found\r\n\r\n";
+
+      socket.getOutputStream().write(op.getBytes());
       System.out.println("accepted new connection");
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());

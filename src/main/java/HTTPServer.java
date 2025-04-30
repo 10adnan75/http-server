c00ad26a5101f3c0f9c063c0053437e81afe16c1
Adnan Mazharuddin Shaikh <adnanmazharuddinshaikh@gmail.com>
2025-04-29 20:57:31 -0700

    final FIX: Return a file (closing the socket doesn't work

--- src/main/java/HTTPServer.java
+++ src/main/java/HTTPServer.java
@@ -81,12 +81,6 @@ public class HTTPServer {
             outputStream.flush();
         } catch (IOException e) {
             throw new RuntimeException(e);
-        } finally {
-            try {
-                clientSocket.close();
-            } catch (IOException e) {
-                System.err.println("Failed to close client socket: " + e.getMessage());
-            }
         }
     }
 }

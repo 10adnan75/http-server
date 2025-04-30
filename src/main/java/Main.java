7cedeb1a7c48dadefddc9dbacd7103645939d30d
Adnan Mazharuddin Shaikh <adnanmazharuddinshaikh@gmail.com>
2025-04-29 20:56:26 -0700

    checking if closing the socket works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        String directory = null;
         final int port = 4221;
         final int threads = 10;
+        String directory = null;
 
         for (int i = 0; i < args.length - 1; i++) {
             if (args[i].equals("--directory")) {
                 directory = args[i + 1];
             }
         }
-
-        // if (directory == null) {
-        //     System.out.println("Directory not provided!");
-        //     return;
-        // }
 
         HTTPServer server = new HTTPServer(port, threads, directory);
         server.run();
